9483636dea715bac52e51339b6c5bbe737483ec0
NitikaMaharjan <maharjanitika@gmail.com>
2026-05-18 16:17:33 +0545

    fetch weather data based on user's location and user's input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import UserLocation from './components/UserLocation'
 
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <UserLocation />
     </>
   )
 }
